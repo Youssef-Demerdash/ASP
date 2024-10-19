8881bfc7dc34cd775005d2611cbcfa5b0a27b37f
Youssef-Demerdash <youssefashrafdem@gmail.com>
2024-10-20 00:37:58 +0300

    classes.php

---
+++ classes.php
@@ -0,0 +1,42 @@
+<?php
+abstract class user{
+    public $firstname;
+    public $lastname;
+    public $Email;
+    public $password;
+    public $role;
+
+    function __construct($firstname,$lastname,$Email,$password,$role){
+        $this->firstname=$firstname;
+        $this->lastname=$lastname;
+        $this->Email=$Email;
+        $this->password=$password;
+        $this->role=$role;
+    }
+}
+
+class student extends user{
+    function __construct($firstname,$lastname,$Email,$password,$role){
+    parent::__construct($firstname, $lastname, $Email, $password, $role);   
+ }
+    
+}
+
+class doctor extends user{
+    function __construct($firstname,$lastname,$Email,$password,$role){
+        parent::__construct($firstname, $lastname, $Email, $password, $role);   
+     }
+}
+
+class TA extends user{
+    function __construct($firstname,$lastname,$Email,$password,$role){
+        parent::__construct($firstname, $lastname, $Email, $password, $role);   
+     }
+}
+
+class admin extends user{
+    function __construct($firstname,$lastname,$Email,$password,$role){
+        parent::__construct($firstname, $lastname, $Email, $password, $role);   
+     }
+}
+?>
