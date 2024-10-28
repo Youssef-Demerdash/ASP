903ccc2b7d69815617d64c202aa71fe8d0ff20b2
Hagarmohamedd00 <hagar2206668@miuegypt.edu.eg>
2024-10-28 20:02:54 +0300

    Update DB.inc.php

--- includes/DB.inc.php
+++ includes/DB.inc.php
@@ -2,11 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-<<<<<<< HEAD
-$DB = "project";
-=======
 $DB = "asp";
->>>>>>> 3fbfe76f8c57f567f75099d0e40e0dada37a875d
 
 $conn = mysqli_connect($servername,$username,$password,$DB);
 
@@ -14,3 +10,4 @@ $conn = mysqli_connect($servername,$username,$password,$DB);
 if(!$conn){
     die("Connection failed: " . mysqli_connect_error());
 }
+?>
